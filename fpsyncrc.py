6579d81e076bb1dd61b85ab3c4df47a6613f5106
Fernando Perez <Fernando.Perez@berkeley.edu>
2019-04-10 23:38:04 -0700

    Updates

--- fpsyncrc.py
+++ fpsyncrc.py
@@ -1,33 +1,33 @@
-"""Configuration file for the laptop-update.py script.  This file should be
-named either ~/.laptop-updaterc.py or ~/usr/etc/laptop-updaterc.py to be found
-by default.  Otherwise its location must be given at runtime as the -config
-option.
+"""
+Configuration file for the laptop-update.py script.  This file should be named
+either `~/.fpsyncrc.py` or `~/usr/etc/fpsyncrc.py` to be found by default.
+Otherwise its location must be given at runtime as the `--config` option.
 
-Some file/directory which must exist for the script to run at all.  Useful to
-prevent it from running if some NFS mount is missing, for example."""
+This file will be `exec`'d by the calling script in a namespace that has the following variables defined by default:
 
-def fexists(arg):
-    """Expand ~ and $ variables in a string and find if it's an existing file"""
+- host
+- start_dir
+- excludes
 
-    import os
+These can be redefined here as desired to set up noew configurations.
+"""
 
-    return os.path.exists(os.path.expanduser(os.path.expandvars(arg)))
+from os.path import join as pjoin
 
-# Otherwise, we default to an ssh channel.  The trailing dot is there to
-# give properly assembled paths starting at $HOME if / is added
-
-server_home = 'SERVER:.'  # BIDS
-excludes = '~/usr/etc/fpsyncrc.excludes'
-
+# Some file/directory which must exist for the script to run at all.  Useful to
+# prevent it from running if some NFS mount is missing, for example.
 
 MUST_EXIST  = '~/.ssh'
+
+server_home = f'{host}:{start_dir}'
+
 
 """
 List of things to update.  It is made of dicts:
 
 dict(dir1 = '~',
-     dir2 = '~/foo',
-     to_update = ('bar','baz'),
+     dir2 = '~/lw',
+     to_update = ('code','research'),
      exclude_from = None,
      )
 
@@ -37,8 +37,8 @@ dict(dir1 = '~',
  directories which must exist in both dir1 and dir2.  They are updated from
  one to the other based on the selected mode: 'up': 1->2, 'down': 2->1.
 
- Note that the entries in to_update CAN NOT have subdirectories in them. If
- you put A/B in your list, B/ will end up at the top level 'dir2' target.  For
+ Note that the entries in to_update CAN NOT have subdirectories in them. If you
+ put A/B in your list, B/ will end up at the top level 'dir2' target.  For
  updating a directory which lives in dir1/A/B to dir2/A/B, make another entry
  in the TO_UPDATE list with only one level.
 
@@ -46,32 +46,75 @@ dict(dir1 = '~',
  to be passed to rsync.
 
  WARNING: because of the semantics of rsync, the files and directories listed
- must NEVER end in '/'.  """
+ must NEVER end in '/'.
+"""
 
-home = dict(dir1 = '~',
-            dir2 = server_home,
+# Main home directory sync, broken up  into several blocks, as I'm getting weird
+# failures that could be caused by the main one having become too large
 
-            to_update = """
-            .bash_profile .bashrc 
-            .emacs .emacs.conf.d .fonts
-            .gitconfig .gitignore .gitk  .hgrc .hgignore
-            .profile .pycheckrc .pypirc .pythonstartup.py .Rprofile
-            .sig .starcluster-completion.sh .ssh .tmux.conf
-            Desktop Documents Pictures
-            """.split(),
-            exclude_from = excludes,
-            )
+h = dict(dir1 = '~',
+         dir2 = server_home,
+         exclude_from = excludes,
+         )
+
+home = h.copy()
+home['to_update'] = """
+                    .bash_profile .bashrc .bashrc_virtualenv
+                    .bash_utils .bash_virtualenv_utils .bash_git_completion
+                    .emacs .emacs.conf.d .fonts
+                    .gitconfig .gitignore .gitk  .hgrc .hgignore
+                    .ipython .inputrc .ispell_english .ispell_spanish
+                    .jed .less .lesskey .lyx
+                    .mrconfig .nose.cfg .noserc
+                    .profile .pycheckrc .pypirc .pythonstartup.py .Rprofile
+                    .sig .starcluster-completion.sh .ssh .tmux.conf 
+                    Desktop Documents Pictures
+                    """.split()
 
 import platform as p
 if p.platform().startswith('Linux'):
     home['to_update'].append('R')
 
-    
-config = dict(dir1 = '~/.config',
-              dir2 = server_home + '/.config',
-              to_update = ['mc'],
+# tmp - dbg
+#home['to_update'] = ['teach']
+
+dirs = """dev ipython jupyter misc prof ref research scratch
+          talks teach texmf usr www
+          """.split()
+
+hdirs = []
+for d in dirs:
+    hd = h.copy()
+    hd['to_update'] = [d]
+    hdirs.append(hd)
+
+# .config directory
+config = dict(dir1 = pjoin('~', '.config'),
+              dir2 = pjoin(server_home, '.config'),
+              to_update = ['mc', 'flake8'],
+              exclude_from = excludes,
+              )
+
+# First-level subdirs of ~/Library
+lib_path = 'Library'
+library = dict(dir1 = pjoin('~', lib_path),
+               dir2 = pjoin(server_home, lib_path),
+               to_update = ['Jupyter'],
+               exclude_from = excludes,
+               )
+
+# VSCode user data. It's in ~/Library, but since it's a nested path, I can't
+# use it
+code_path = 'Library/Application Support/Code'
+vscode = dict(dir1 = pjoin('~', code_path),
+              dir2 = pjoin(server_home, code_path),
+              to_update = ['User'],
               exclude_from = excludes,
               )
 
 # Create the list of configs to use in the update process
-TO_UPDATE = [config, home]
+TO_UPDATE = [config,
+             library,
+#             vscode,
+             home,
+             ] + hdirs
